89a82e5e93dc4d1e429653b38bf0e194ea63491b
vladislavkovalskyi <v.darknesssb@gmail.com>
2023-05-03 01:29:53 +0300

    End.

--- app.c
+++ app.c
@@ -4,17 +4,17 @@
 
 #define TRAINS_PATH "trains.txt"
 
-// Структура данных train
+// РЎС‚СЂСѓРєС‚СѓСЂР° РґР°РЅРЅС‹С… train
 struct train {
-  char destination[50];     // Место назначения
-  int train_number;         // Номер поезда
-  char departure_time[10];  // Время отправления
-  char arrival_time[10];    // Время прибытия
-  double ticket_price;      // Цена билета
+  char destination[50];     // РњРµСЃС‚Рѕ РЅР°Р·РЅР°С‡РµРЅРёСЏ
+  int train_number;         // РќРѕРјРµСЂ РїРѕРµР·РґР°
+  char departure_time[10];  // Р’СЂРµРјСЏ РѕС‚РїСЂР°РІР»РµРЅРёСЏ
+  char arrival_time[10];    // Р’СЂРµРјСЏ РїСЂРёР±С‹С‚РёСЏ
+  double ticket_price;      // Р¦РµРЅР° Р±РёР»РµС‚Р°
   struct train* next;
 };
 
-// Функция для очистки терминала
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РѕС‡РёСЃС‚РєРё С‚РµСЂРјРёРЅР°Р»Р°
 void clear_terminal() {
 #ifdef _WIN32
   system("cls");
@@ -22,11 +22,11 @@ void clear_terminal() {
   system("clear");
 #else
   printf(ERROR_TEXT
-         "Ви скомпілювали код на невідомій ОС. Очищення терміналу неможливе\n");
+         "Р’Рё СЃРєРѕРјРїС–Р»СЋРІР°Р»Рё РєРѕРґ РЅР° РЅРµРІС–РґРѕРјС–Р№ РћРЎ. РћС‡РёС‰РµРЅРЅСЏ С‚РµСЂРјС–РЅР°Р»Сѓ РЅРµРјРѕР¶Р»РёРІРµ\n");
 #endif
 }
 
-// DEV: Функция для добавления поезда
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РґРѕР±Р°РІР»РµРЅРёСЏ РїРѕРµР·РґР°
 void add_train(struct train** head, struct train new_train) {
   struct train* new_node = (struct train*)malloc(sizeof(struct train));
 
@@ -35,24 +35,25 @@ void add_train(struct train** head, struct train new_train) {
   *head = new_node;
 }
 
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РЅР°РїРёСЃР°РЅРёСЏ РїРѕРµР·РґР°
 void print_train(struct train one_train, int number) {
   fflush(stdin);
   printf(
-      "• (#%d) Інформація про розклад\n"
-      "Місце признач.:\t%s\n"
-      "Номер потягу:\t%d\n"
-      "Час відправки:\t%s\n"
-      "Час прибуття:\t%s\n"
-      "Ціна білету:\t%.2f\n\n",
+      "вЂў (#%d) Р†РЅС„РѕСЂРјР°С†С–СЏ РїСЂРѕ СЂРѕР·РєР»Р°Рґ\n"
+      "РњС–СЃС†Рµ РїСЂРёР·РЅР°С‡.:\t%s\n"
+      "РќРѕРјРµСЂ РїРѕС‚СЏРіСѓ:\t%d\n"
+      "Р§Р°СЃ РІС–РґРїСЂР°РІРєРё:\t%s\n"
+      "Р§Р°СЃ РїСЂРёР±СѓС‚С‚СЏ:\t%s\n"
+      "Р¦С–РЅР° Р±С–Р»РµС‚Сѓ:\t%.2f\n\n",
       number, one_train.destination, one_train.train_number,
       one_train.departure_time, one_train.arrival_time, one_train.ticket_price);
 }
 
-// Функция для записи данных из стека в файл
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ Р·Р°РїРёСЃРё РґР°РЅРЅС‹С… РёР· СЃС‚РµРєР° РІ С„Р°Р№Р»
 void save_to_file(struct train* head, const char* filename) {
   FILE* fp = fopen(filename, "w");
   if (fp == NULL) {
-    printf("Не удалось открыть файл для записи\n");
+    printf("РќРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р» РґР»СЏ Р·Р°РїРёСЃРё\n");
     return;
   }
   while (head != NULL) {
@@ -64,11 +65,11 @@ void save_to_file(struct train* head, const char* filename) {
 }
 ////////////////////////////////////////////////////
 
-// Функция для считывания данных из файла и записи их в стек
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ СЃС‡РёС‚С‹РІР°РЅРёСЏ РґР°РЅРЅС‹С… РёР· С„Р°Р№Р»Р° Рё Р·Р°РїРёСЃРё РёС… РІ СЃС‚РµРє
 struct train* load_from_file(const char* filename) {
   FILE* fp = fopen(filename, "r");
   if (fp == NULL) {
-    printf("Не удалось открыть файл для чтения\n");
+    printf("РќРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р» РґР»СЏ С‡С‚РµРЅРёСЏ\n");
     return NULL;
   }
   struct train* head = NULL;
@@ -97,96 +98,98 @@ struct train* load_from_file(const char* filename) {
     }
   }
   fclose(fp);
-  printf("Данные загружены из файла %s\n", filename);
+  printf("Р”Р°РЅРЅС‹Рµ Р·Р°РіСЂСѓР¶РµРЅС‹ РёР· С„Р°Р№Р»Р° %s\n", filename);
   return head;
 }
 
+// Р¤СѓРЅРєС†РёСЏ РґРѕР±Р°РІР»РµРЅРёСЏ РЅРѕРІРѕРіРѕ РїРѕРµР·РґР°
 void add_new_train(struct train** head) {
   struct train new_train;
   while (getchar() != '\n')
-    ;  // Считываем и удаляем все символы из входного буфера
-  printf("Введіть інформацію про новий поїзд:\n");
-  printf("• Місце призначення: ");
+    ;  // РЎС‡РёС‚С‹РІР°РµРј Рё СѓРґР°Р»СЏРµРј РІСЃРµ СЃРёРјРІРѕР»С‹ РёР· РІС…РѕРґРЅРѕРіРѕ Р±СѓС„РµСЂР°
+  printf("Р’РІРµРґС–С‚СЊ С–РЅС„РѕСЂРјР°С†С–СЋ РїСЂРѕ РЅРѕРІРёР№ РїРѕС—Р·Рґ:\n");
+  printf("вЂў РњС–СЃС†Рµ РїСЂРёР·РЅР°С‡РµРЅРЅСЏ: ");
   scanf("%s", new_train.destination);
-  printf("• Номер поїзду: ");
+  printf("вЂў РќРѕРјРµСЂ РїРѕС—Р·РґСѓ: ");
   scanf("%d", &new_train.train_number);
-  printf("• Час відправлення: ");
+  printf("вЂў Р§Р°СЃ РІС–РґРїСЂР°РІР»РµРЅРЅСЏ: ");
   scanf("%s", new_train.departure_time);
-  printf("• Час прибуття: ");
+  printf("вЂў Р§Р°СЃ РїСЂРёР±СѓС‚С‚СЏ: ");
   scanf("%s", new_train.arrival_time);
-  printf("• Ціна білету: ");
+  printf("вЂў Р¦С–РЅР° Р±С–Р»РµС‚Сѓ: ");
   scanf("%lf", &new_train.ticket_price);
 
   add_train(head, new_train);
   clear_terminal();
   // save_to_file(*head, TRAINS_PATH);
-  printf("• Новий рейс був успішно доданий та збережений в файл %s\n",
+  printf("вЂў РќРѕРІРёР№ СЂРµР№СЃ Р±СѓРІ СѓСЃРїС–С€РЅРѕ РґРѕРґР°РЅРёР№ С‚Р° Р·Р±РµСЂРµР¶РµРЅРёР№ РІ С„Р°Р№Р» %s\n",
          TRAINS_PATH);
 }
 
-// Удаление рейса
+// РЈРґР°Р»РµРЅРёРµ СЂРµР№СЃР°
 void delete_train(struct train** head) {
   struct train* current = *head;
   struct train* previous = NULL;
   int i = 1;
   int number = 0;
-  printf("• Вкажіть номер розкладу: ");
+  printf("вЂў Р’РєР°Р¶С–С‚СЊ РЅРѕРјРµСЂ СЂРѕР·РєР»Р°РґСѓ: ");
   scanf("%d", &number);
 
-  if (number == 1) {  // Если удаляем первый элемент
+  if (number == 1) {  // Р•СЃР»Рё СѓРґР°Р»СЏРµРј РїРµСЂРІС‹Р№ СЌР»РµРјРµРЅС‚
     *head = current->next;
     free(current);
     return;
   }
 
-  while (i < number && current != NULL) {  // Поиск элемента по номеру
+  while (i < number && current != NULL) {  // РџРѕРёСЃРє СЌР»РµРјРµРЅС‚Р° РїРѕ РЅРѕРјРµСЂСѓ
     previous = current;
     current = current->next;
     i++;
   }
 
-  if (current == NULL) {  // Если элемент не найден
+  if (current == NULL) {  // Р•СЃР»Рё СЌР»РµРјРµРЅС‚ РЅРµ РЅР°Р№РґРµРЅ
     printf("Element with number %d not found.\n", number);
     return;
   }
 
-  previous->next = current->next;  // Удаление элемента
+  previous->next = current->next;  // РЈРґР°Р»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р°
   // save_to_file(*head, TRAINS_PATH);
   free(current);
 }
 
-// Редактирование рейса
+// Р РµРґР°РєС‚РёСЂРѕРІР°РЅРёРµ СЂРµР№СЃР°
 void edit_element_by_number(struct train* head) {
   struct train* current = head;
   int i = 1;
 
   int number = 0;
-  printf("• Вкажіть номер розкладу: ");
+  printf("вЂў Р’РєР°Р¶С–С‚СЊ РЅРѕРјРµСЂ СЂРѕР·РєР»Р°РґСѓ: ");
   scanf("%d", &number);
 
-  while (i < number && current != NULL) {  // Поиск элемента по номеру
+  while (i < number && current != NULL) {  // РџРѕРёСЃРє СЌР»РµРјРµРЅС‚Р° РїРѕ РЅРѕРјРµСЂСѓ
     current = current->next;
     i++;
   }
 
-  if (current == NULL) {  // Если элемент не найден
-    printf("• Розкладу з номером #%d не знайдено.\n", number);
+  if (current == NULL) {  // Р•СЃР»Рё СЌР»РµРјРµРЅС‚ РЅРµ РЅР°Р№РґРµРЅ
+    printf("вЂў Р РѕР·РєР»Р°РґСѓ Р· РЅРѕРјРµСЂРѕРј #%d РЅРµ Р·РЅР°Р№РґРµРЅРѕ.\n", number);
     return;
   }
 
-  // Редактирование элемента
-  printf("• Місце призначення: ");
+  // Р РµРґР°РєС‚РёСЂРѕРІР°РЅРёРµ СЌР»РµРјРµРЅС‚Р°
+  printf("вЂў РњС–СЃС†Рµ РїСЂРёР·РЅР°С‡РµРЅРЅСЏ: ");
   scanf("%s", current->destination);
-  printf("• Номер поїзду: ");
+  printf("вЂў РќРѕРјРµСЂ РїРѕС—Р·РґСѓ: ");
   scanf("%d", &current->train_number);
-  printf("• Час відправлення: ");
+  printf("вЂў Р§Р°СЃ РІС–РґРїСЂР°РІР»РµРЅРЅСЏ: ");
   scanf("%s", current->departure_time);
-  printf("• Час прибуття: ");
+  printf("вЂў Р§Р°СЃ РїСЂРёР±СѓС‚С‚СЏ: ");
   scanf("%s", current->arrival_time);
-  printf("• Ціна білету: ");
+  printf("вЂў Р¦С–РЅР° Р±С–Р»РµС‚Сѓ: ");
   scanf("%lf", &current->ticket_price);
 }
 
+// Р¤СѓРЅРєС†РёСЏ РЅР°РїРёСЃР°РЅРёСЏ РІСЃРµС… РїРѕРµР·РґРѕРІ
 void print_all_trains(struct train* head) {
   struct train* current = head;
   int number = 0;
@@ -197,87 +200,91 @@ void print_all_trains(struct train* head) {
   }
 }
 
+// Р¤СѓРЅРєС†РёСЏ РЅР°РїРёСЃР°РЅРёСЏ РїРѕРµР·РґРѕРІ РїРѕ СѓРєР°Р·Р°РЅРЅРѕРјСѓ РЅР°РїСЂР°РІР»РµРЅРёСЋ
 void show_train_by_destination(struct train* head) {
   struct train* current = head;
   char destination[50];
   int found = 0;
-  printf("• Вкажіть напрямок: ");
+  printf("вЂў Р’РєР°Р¶С–С‚СЊ РЅР°РїСЂСЏРјРѕРє: ");
   scanf("%49s", destination);
 
   while (current != NULL) {
     if (strcmp(current->destination, destination) == 0) {
-      printf("Місце признач.:\t%s\n", current->destination);
-      printf("Номер потягу:\t%d\n", current->train_number);
-      printf("Час відправки:\t%s\n", current->departure_time);
-      printf("Час прибуття:\t%s\n", current->arrival_time);
-      printf("Ціна білету:\t%.2lf\n\n", current->ticket_price);
+      printf("РњС–СЃС†Рµ РїСЂРёР·РЅР°С‡.:\t%s\n", current->destination);
+      printf("РќРѕРјРµСЂ РїРѕС‚СЏРіСѓ:\t%d\n", current->train_number);
+      printf("Р§Р°СЃ РІС–РґРїСЂР°РІРєРё:\t%s\n", current->departure_time);
+      printf("Р§Р°СЃ РїСЂРёР±СѓС‚С‚СЏ:\t%s\n", current->arrival_time);
+      printf("Р¦С–РЅР° Р±С–Р»РµС‚Сѓ:\t%.2lf\n\n", current->ticket_price);
       found = 1;
     }
     current = current->next;
   }
   if (!found) {
-    printf("Напрямку \"%s\" не знайдено.\n",
-           destination);  // Если направление не найдено
+    printf("РќР°РїСЂСЏРјРєСѓ \"%s\" РЅРµ Р·РЅР°Р№РґРµРЅРѕ.\n",
+           destination);  // Р•СЃР»Рё РЅР°РїСЂР°РІР»РµРЅРёРµ РЅРµ РЅР°Р№РґРµРЅРѕ
   }
 }
 
+// Р¤СѓРЅРєС†РёСЏ, РєРѕС‚РѕСЂР°СЏ РїРµС‡Р°С‚Р°РµС‚ РїРѕРµР·РґР° РїРѕ РЅРѕРјРµСЂСѓ РїРѕРµР·РґР° Рё РјРµСЃС‚Сѓ
 void show_train_by_number_and_destination(struct train* head) {
   struct train* current = head;
   int train_number;
   char destination[50];
-  printf("• Введіть номер потягу: ");
+  printf("вЂў Р’РІРµРґС–С‚СЊ РЅРѕРјРµСЂ РїРѕС‚СЏРіСѓ: ");
   scanf("%d", &train_number);
-  printf("• Введіть станцію призначення: ");
+  printf("вЂў Р’РІРµРґС–С‚СЊ СЃС‚Р°РЅС†С–СЋ РїСЂРёР·РЅР°С‡РµРЅРЅСЏ: ");
   scanf("%49s", destination);
   int train_found = 0;
 
   while (current != NULL) {
     if (current->train_number == train_number &&
         strcmp(current->destination, destination) == 0) {
-      printf("• Час прибуття:\t%s\n", current->arrival_time);
+      printf("вЂў Р§Р°СЃ РїСЂРёР±СѓС‚С‚СЏ:\t%s\n", current->arrival_time);
       train_found = 1;
     }
     current = current->next;
   }
 
   if (!train_found) {
-    printf("Потяг з номером %d та напрямком \"%s\" не знайдено.\n",
+    printf("РџРѕС‚СЏРі Р· РЅРѕРјРµСЂРѕРј %d С‚Р° РЅР°РїСЂСЏРјРєРѕРј \"%s\" РЅРµ Р·РЅР°Р№РґРµРЅРѕ.\n",
            train_number, destination);
   }
 }
 
+// РџРѕРєР°Р·С‹РІР°РµС‚ РёРЅС„Рѕ Рѕ РїРѕРµР·РґРµ РїРѕ РµРіРѕ РЅРѕРјРµСЂСѓ
 void show_train_info(struct train* head) {
   int train_number;
-  printf("• Вкажіть номер потягу: ");
+  printf("вЂў Р’РєР°Р¶С–С‚СЊ РЅРѕРјРµСЂ РїРѕС‚СЏРіСѓ: ");
   scanf("%d", &train_number);
   int found = 0;
   struct train* current = head;
   while (current != NULL) {
     if (current->train_number == train_number) {
-      printf("• Час відправ.:\t%s\n", current->departure_time);
-      printf("• Ціна білету:\t%.2lf грн\n\n", current->ticket_price);
+      printf("вЂў Р§Р°СЃ РІС–РґРїСЂР°РІ.:\t%s\n", current->departure_time);
+      printf("вЂў Р¦С–РЅР° Р±С–Р»РµС‚Сѓ:\t%.2lf РіСЂРЅ\n\n", current->ticket_price);
       found = 1;
     }
     current = current->next;
   }
   if (!found) {
-    printf("Потяг з номером %d не знайдено.\n", train_number);
+    printf("РџРѕС‚СЏРі Р· РЅРѕРјРµСЂРѕРј %d РЅРµ Р·РЅР°Р№РґРµРЅРѕ.\n", train_number);
   }
 }
 
+// РЈРґР°Р»СЏРµС‚ РїРѕРµР·РґР° РїРѕ РЅР°РїСЂР°РІР»РµРЅРёСЋ
 void delete_trains_by_destination(struct train** head) {
   char destination[50];
-  printf("• Вкажіть напрямок: ");
+  printf("вЂў Р’РєР°Р¶С–С‚СЊ РЅР°РїСЂСЏРјРѕРє: ");
   scanf("%49s", destination);
 
   struct train* current = *head;
   struct train* prev = NULL;
 
-  // Перебираем все элементы списка
+  // РџРµСЂРµР±РёСЂР°РµРј РІСЃРµ СЌР»РµРјРµРЅС‚С‹ СЃРїРёСЃРєР°
   while (current != NULL) {
-    // Если направление совпадает, удаляем поезд
+    // Р•СЃР»Рё РЅР°РїСЂР°РІР»РµРЅРёРµ СЃРѕРІРїР°РґР°РµС‚, СѓРґР°Р»СЏРµРј РїРѕРµР·Рґ
     if (strcmp(current->destination, destination) == 0) {
-      // Если удаляемый элемент первый в списке
+      // Р•СЃР»Рё СѓРґР°Р»СЏРµРјС‹Р№ СЌР»РµРјРµРЅС‚ РїРµСЂРІС‹Р№ РІ СЃРїРёСЃРєРµ
       if (prev == NULL) {
         *head = current->next;
       } else {
@@ -285,31 +292,74 @@ void delete_trains_by_destination(struct train** head) {
       }
       struct train* temp = current;
       current = current->next;
-      free(temp);  // Освобождаем память удаленного элемента
+      free(temp);  // РћСЃРІРѕР±РѕР¶РґР°РµРј РїР°РјСЏС‚СЊ СѓРґР°Р»РµРЅРЅРѕРіРѕ СЌР»РµРјРµРЅС‚Р°
     } else {
       prev = current;
       current = current->next;
     }
   }
-  printf("• Всі рейси за напрямком \"%s\" видалені.\n", destination);
+  printf("вЂў Р’СЃС– СЂРµР№СЃРё Р·Р° РЅР°РїСЂСЏРјРєРѕРј \"%s\" РІРёРґР°Р»РµРЅС–.\n", destination);
 }
 
-// Предметна область – залізничний вокзал.
-// Реалізувати динамічну структуру даних (стек), що містить інформацію про
-// розклад руху поїздів по певній станції: станція призначення; номер поїзда;
-// час відправлення; час прибуття на станцію призначення; вартість проїзду.
-// Програма повинна забезпечувати:
-// – додавання елемента; +
-// – видалення елемента; +
-// – можливість коригування даних; +
-// – виведення всіх даних; +
-// – видачу довідки про всі поїзди зазначеного напрямку;. +
-// – видачу довідки про час прибуття кожного з поїздів зазначеного напрямку на
-// станцію призначення; +
-// – видачу довідки про час відправлення поїзда з заданим
-// номером; +
-// – видалення з бази записів про потяги зазначеного напрямку; +
-// – сортування за часом відправлення
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ СЃРІР°Р№РїР° СЃС‚РµРєРѕРІ
+void swap_nodes(struct train* node1, struct train* node2) {
+  char temp_destination[50];
+  int temp_train_number;
+  char temp_departure_time[10];
+  char temp_arrival_time[10];
+  double temp_ticket_price;
+
+  // РЎРѕС…СЂР°РЅСЏРµРј Р·РЅР°С‡РµРЅРёСЏ РїРѕР»РµР№ РїРµСЂРІРѕРіРѕ СѓР·Р»Р°
+  strcpy(temp_destination, node1->destination);
+  temp_train_number = node1->train_number;
+  strcpy(temp_departure_time, node1->departure_time);
+  strcpy(temp_arrival_time, node1->arrival_time);
+  temp_ticket_price = node1->ticket_price;
+
+  // РљРѕРїРёСЂСѓРµРј Р·РЅР°С‡РµРЅРёСЏ РїРѕР»РµР№ РІС‚РѕСЂРѕРіРѕ СѓР·Р»Р° РІ РїРµСЂРІС‹Р№ СѓР·РµР»
+  strcpy(node1->destination, node2->destination);
+  node1->train_number = node2->train_number;
+  strcpy(node1->departure_time, node2->departure_time);
+  strcpy(node1->arrival_time, node2->arrival_time);
+  node1->ticket_price = node2->ticket_price;
+
+  // РљРѕРїРёСЂСѓРµРј СЃРѕС…СЂР°РЅРµРЅРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ РїРѕР»РµР№ РїРµСЂРІРѕРіРѕ СѓР·Р»Р° РІРѕ РІС‚РѕСЂРѕР№ СѓР·РµР»
+  strcpy(node2->destination, temp_destination);
+  node2->train_number = temp_train_number;
+  strcpy(node2->departure_time, temp_departure_time);
+  strcpy(node2->arrival_time, temp_arrival_time);
+  node2->ticket_price = temp_ticket_price;
+}
+
+// Р¤СѓРЅРєС†РёСЏ, РєРѕС‚РѕСЂР°СЏ СЃРѕСЂС‚РёСЂСѓРµС‚ РїРѕРµР·РґР° РїРѕ РІСЂРµРјРµРЅРё
+void sort_trains_by_time(struct train* head) {
+  struct train* current;
+  struct train* next_node;
+  int swapped;
+
+  // Р•СЃР»Рё СЃРїРёСЃРѕРє РїСѓСЃС‚ РёР»Рё СЃРѕСЃС‚РѕРёС‚ РёР· РѕРґРЅРѕРіРѕ СЌР»РµРјРµРЅС‚Р°, СЃРѕСЂС‚РёСЂРѕРІРєР° РЅРµ РЅСѓР¶РЅР°
+  if (head == NULL || head->next == NULL) {
+    return;
+  }
+
+  do {
+    swapped = 0;
+    current = head;
+
+    while (current->next != NULL) {
+      next_node = current->next;
+
+      // Р•СЃР»Рё РІСЂРµРјСЏ СЃР»РµРґСѓСЋС‰РµРіРѕ СѓР·Р»Р° РјРµРЅСЊС€Рµ РІСЂРµРјРµРЅРё С‚РµРєСѓС‰РµРіРѕ СѓР·Р»Р°, РјРµРЅСЏРµРј РёС… РјРµСЃС‚Р°РјРё
+      if (strcmp(current->departure_time, next_node->departure_time) > 0) {
+        swap_nodes(current, next_node);
+        swapped = 1;
+      }
+
+      current = next_node;
+    }
+  } while (swapped);
+}
+
 int main() {
   SetConsoleCP(1251);
   SetConsoleOutputCP(1251);
@@ -320,17 +370,18 @@ int main() {
     fflush(stdin);
 
     printf(
-        "• Меню залізничного вокзалу:\n"
-        "1. Додати новий рейс\n"
-        "2. Видалити рейс\n"
-        "3. Редагувати рейс\n"
-        "4. Вивести всі рейси\n"
-        "5. Вивести рейси за напрямком\n"
-        "6. Вивести час прибуття за потягом та місцем призначення\n"
-        "7. Вивести інфо. за номером потягу\n"
-        "8. Видалити рейси за напрямком\n"
-        "9. Вихід\n"
-        "• Введіть номер дії: ");
+        "вЂў РњРµРЅСЋ Р·Р°Р»С–Р·РЅРёС‡РЅРѕРіРѕ РІРѕРєР·Р°Р»Сѓ:\n"
+        "1. Р”РѕРґР°С‚Рё РЅРѕРІРёР№ СЂРµР№СЃ\n"
+        "2. Р’РёРґР°Р»РёС‚Рё СЂРµР№СЃ\n"
+        "3. Р РµРґР°РіСѓРІР°С‚Рё СЂРµР№СЃ\n"
+        "4. Р’РёРІРµСЃС‚Рё РІСЃС– СЂРµР№СЃРё\n"
+        "5. Р’РёРІРµСЃС‚Рё СЂРµР№СЃРё Р·Р° РЅР°РїСЂСЏРјРєРѕРј\n"
+        "6. Р’РёРІРµСЃС‚Рё С‡Р°СЃ РїСЂРёР±СѓС‚С‚СЏ Р·Р° РїРѕС‚СЏРіРѕРј С‚Р° РјС–СЃС†РµРј РїСЂРёР·РЅР°С‡РµРЅРЅСЏ\n"
+        "7. Р’РёРІРµСЃС‚Рё С–РЅС„Рѕ. Р·Р° РЅРѕРјРµСЂРѕРј РїРѕС‚СЏРіСѓ\n"
+        "8. Р’РёРґР°Р»РёС‚Рё СЂРµР№СЃРё Р·Р° РЅР°РїСЂСЏРјРєРѕРј\n"
+        "9. Р’С–РґСЃРѕСЂС‚СѓРІР°С‚Рё СЂРµР№СЃРё Р·Р° С‡Р°СЃРѕРј\n"
+        "10. Р’РёС…С–Рґ\n"
+        "вЂў Р’РІРµРґС–С‚СЊ РЅРѕРјРµСЂ РґС–С—: ");
     scanf("%d", &choice);
     switch (choice) {
       case 1:
@@ -358,11 +409,15 @@ int main() {
         delete_trains_by_destination(&trains);
         break;
       case 9:
-        printf("• До побачення!\n");
+        sort_trains_by_time(trains);
+        print_all_trains(trains);
+        break;
+      case 10:
+        printf("вЂў Р”Рѕ РїРѕР±Р°С‡РµРЅРЅСЏ!\n");
         exit(0);
         break;
       default:
-        printf("• Ви ввели невідому дію.\n");
+        printf("вЂў Р’Рё РІРІРµР»Рё РЅРµРІС–РґРѕРјСѓ РґС–СЋ.\n");
         break;
     }
     save_to_file(trains, TRAINS_PATH);
